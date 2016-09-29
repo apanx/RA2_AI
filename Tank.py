3c8cf61aabf6ae02f1110f305d02c61f4667469b
apanx <apanx@gtm>
2016-09-29 18:27:24 +0200

    Refactor Tank, FlameTank, MGTank
    FlameTank now has always-on FlameThrower

--- Tank.py
+++ Tank.py
@@ -57,17 +57,23 @@ class Tank(AI.SuperAI):
     def __init__(self, **args):
         AI.SuperAI.__init__(self, **args)
         self.triggers = ["Fire"]
-        self.Motor = 1
-        self.DSL = 1
-        if 'triggers' in args: self.triggers = args['triggers']
-        if 'motor' in args: self.Motor = args['motor']
-        if 'DSL' in args: self.DSL = args['DSL']
+        self.motor = 1
+        self.motor_direction = 1
+        self.inverted_servo = False
+        self.tickInterval = 0.05
+        self.tickInterval_factor = 0.125 / self.tickInterval
 
+        if 'triggers' in args: self.triggers = args['triggers']
+        if 'motor' in args: self.motor = args['motor']
+        if 'inverted_servo' in args:
+            if args['inverted_servo']:
+                self.motor_direction = -1;
+        if 'tickInterval' in args: self.tickInterval = args['tickInterval']
         self.triggerIterator = iter(self.triggers)
         self.tactics.append(Annoy(self))
 
     def Activate(self, active):
-        self.tickInterval = 0.05
+
         if active:
             if AI.SuperAI.debugging:
                 self.debug = Gooey.Plain("watch", 0, 150, 250, 165)
@@ -104,15 +110,15 @@ class Tank(AI.SuperAI):
 
         for trigger in self.triggers:
             self.Input(trigger, 0, 0)
+
         Target, range = self.GetNearestEnemy()
         if Target != None:
-            Winkel = (self.GetHeadingToID(Target, False) + self.GetMotorAngle(self.Motor) * self.DSL)
+            Winkel = (self.GetHeadingToID(Target, False) + self.GetMotorAngle(self.motor) * self.motor_direction)
             self.DebugString(6, str(self.GetHeadingToID(Target, False)))
-            self.DebugString(7, str(-self.GetMotorAngle(self.Motor)))
+            self.DebugString(7, str(-self.GetMotorAngle(self.motor)))
             self.DebugString(8, str(Winkel))
             if self.AimTowards(Winkel):
-                for trigger in self.triggers:
-                    self.Input(trigger, 0, 1)
+                self.FireGun();
 
         return AI.SuperAI.Tick(self)
 
@@ -136,7 +142,7 @@ class Tank(AI.SuperAI):
         self.Input('Aim', 0, level)
         self.DebugString(9, "Aim = " + str(int(level)))
 
-    def AimTowards(self, heading, in_reverse = False):
+    def AimTowards(self, heading):
         THRESHOLD = .05
         dreh = 0
 
@@ -157,17 +163,21 @@ class Tank(AI.SuperAI):
             self.Aim(0)
             return True
 
+    def FireGun(self):
+        for trigger in self.triggers:
+            self.Input(trigger, 0, 1)
+
     def StuckHandler(self):
         "This default generator is called when the bot is almost immobile."
         while 1:
             # back up for 2 seconds (will stop once we're not immobile)
-            for i in range(0, 40):
+            for i in range(0, 16 * self.tickInterval_factor):
                 pos = vector3(self.GetLocation())
                 dir = vector3(self.GetDirection())
                 self.DriveToLocation((pos - dir * 3).asTuple(), True)
                 yield 0
             # go forward for 2 seconds
-            for i in range(0, 40):
+            for i in range(0, 16 * self.tickInterval_factor):
                 pos = vector3(self.GetLocation())
                 dir = vector3(self.GetDirection())
                 self.DriveToLocation((pos + dir * 3).asTuple())
@@ -178,110 +188,38 @@ class Tank(AI.SuperAI):
         while 1:
             self.Input("SRM", 0, 1)
 
-            for i in range(0, 40):
+            for i in range(0, 16 * self.tickInterval_factor):
                 yield 0
 
 class FlameTank(Tank):
-    "Tank with turret and frontal flamethrower"
+    "Tank with turret and always-on frontal flamethrower"
     name = "FlameTank"
 
     def __init__(self, **args):
-        AI.SuperAI.__init__(self, **args)
-        self.zone = "weapon"
-        self.triggers = ["Fire"]
+        Tank.__init__(self, **args)
         self.secondary = ["Shoot"]
-        self.Motor = 1
-        if 'triggers' in args: self.triggers = args['triggers']
-        if 'motor' in args: self.Motor = args['motor']
         if 'secondary' in args: self.secondary = args['secondary']
-
+        tactic = [x for x in self.tactics if x.name == "Annoy"]
+        self.tactics.remove(tactic[0])
+        self.tactics.append(Tactics.Engage(self))
 
     def Activate(self, active):
-        if active:
-            if AI.SuperAI.debugging:
-                self.debug = Gooey.Plain("watch", 0, 150, 250, 165)
-                tbox = self.debug.addText("line0", 0, 0, 250, 15)
-                tbox.setText("Throttle")
-                tbox = self.debug.addText("line1", 0, 15, 250, 15)
-                tbox.setText("Turning")
-                tbox = self.debug.addText("line2", 0, 30, 250, 15)
-                tbox.setText("")
-                tbox = self.debug.addText("line3", 0, 45, 250, 15)
-                tbox.setText("")
-                tbox = self.debug.addText("line4", 0, 60, 250, 15)
-                tbox.setText("")
-                tbox = self.debug.addText("line5", 0, 75, 250, 15)
-                tbox.setText("")
-                tbox = self.debug.addText("line6", 0, 90, 250, 15)
-                tbox.setText("")
-                tbox = self.debug.addText("line7", 0, 105, 250, 15)
-                tbox.setText("")
-                tbox = self.debug.addText("line8", 0, 120, 250, 15)
-                tbox.setText("")
-                tbox = self.debug.addText("line9", 0, 135, 250, 15)
-                tbox.setText("")
-                self.RegisterSmartZone(self.zone, 1)
-
-        return AI.SuperAI.Activate(self, active)
-
-    def Tick(self):
-        if AI.SuperAI.debugging:
-            speed = self.GetSpeed()
-            self.DebugString(4, "Speed = " + str(speed))
-
-            turning_speed = self.GetTurning()
-            self.DebugString(5, "TSpeed = " + str(turning_speed))
-
         for trigger in self.secondary:
-            self.Input(trigger, 0, 0)
-        for trigger in self.triggers:
-            self.Input(trigger, 0, 0)
-        Target, range = self.GetNearestEnemy()
-        if Target != None:
-            Winkel = (self.GetHeadingToID(Target, False) + self.GetMotorAngle(self.Motor) * self.DSL)
-            self.DebugString(6, str(self.GetHeadingToID(Target, False)))
-            self.DebugString(7, str(-self.GetMotorAngle(self.Motor)))
-            self.DebugString(8, str(Winkel))
-            if self.AimTowards(Winkel):
-                for trigger in self.triggers:
-                    self.Input(trigger, 0, 1)
-        if self.weapons:
-            targets = [x for x in self.sensors.itervalues() if x.contacts > 0 \
-            and not plus.isDefeated(x.robot)]
+            self.Input(trigger, 0, 100)
 
-        if len(targets) > 0:
-                for trigger in self.secondary:
-                    self.Input(trigger, 0, 100)
-
-        return AI.SuperAI.Tick(self)
+        return Tank.Activate(self, active)
 
 class MGTank(Tank):
     "Machinegun Tank"
     name = "MGTank"
 
-    def Tick(self):
-        if AI.SuperAI.debugging:
-            speed = self.GetSpeed()
-            self.DebugString(4, "Speed = " + str(speed))
-
-            turning_speed = self.GetTurning()
-            self.DebugString(5, "TSpeed = " + str(turning_speed))
-
-        Target, range = self.GetNearestEnemy()
-        if Target != None:
-            Winkel = (self.GetHeadingToID(Target, False) + self.GetMotorAngle(self.Motor) * self.DSL)
-            self.DebugString(6, str(self.GetHeadingToID(Target, False)))
-            self.DebugString(7, str(-self.GetMotorAngle(self.Motor)))
-            self.DebugString(8, str(Winkel))
-            self.AimTowards(Winkel)
-            try:
-                    trigger = self.triggerIterator.next()
-            except StopIteration:
-                    self.triggerIterator = iter(self.triggers)
-                    trigger = self.triggerIterator.next()
-            self.Input(trigger, 0, 1)
-
-        return AI.SuperAI.Tick(self)
+    def FireGun(self):
+        try:
+            trigger = self.triggerIterator.next()
+        except StopIteration:
+            self.triggerIterator = iter(self.triggers)
+            trigger = self.triggerIterator.next()
+        self.Input(trigger, 0, 1)
 
 AI.register(Tank)
 AI.register(FlameTank)
